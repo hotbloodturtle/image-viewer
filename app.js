051c90a1ebed87ba6d24d89ddd40b0321af3dec5
Bale-haeseok <hs.kang@fitpet.co.kr>
2021-11-09 21:04:43 +0900

    fix - 간결화

--- app.js
+++ app.js
@@ -4,19 +4,14 @@ const os = require("os");
 
 const {
   readFile,
-  readdirSync,
   readdir,
   existsSync,
-  mkdirSync,
   createReadStream,
-  rename,
 } = require("fs");
 
-const getAnimals = (source) => {
-  return readdirSync(source, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
-};
+const ROOT_PATH = '/Users/kanghaeseok/storage/emulated/0'; // todo
+const ANIMAL_PATH = ROOT_PATH + '/DCIM/Screenshots'
+const CHARACTER_PATH = ROOT_PATH + '/Download'
 
 const getFiles = (source) => {
   return new Promise((resolve, reject) => {
@@ -27,7 +22,7 @@ const getFiles = (source) => {
         resolve(
           files
             .filter((file) => file.isFile() && file.name !== ".DS_Store")
-            .map((file) => `${source}/${file.name}`)
+            .map((file) => `${file.name}`)
         );
       }
     );
@@ -43,17 +38,6 @@ const getIndexPage = () => {
   });
 };
 
-const getAnimalsFiles = (response, characters, dirs) => {
-  let animals = [];
-  const results = dirs.map((dir) => getFiles(`photo/reference/animal/${dir}`));
-  Promise.all(results).then((values) => {
-    values.forEach((value) => animals.push(...value));
-    response.writeHead(200);
-    response.write(JSON.stringify({ animals, characters }));
-    response.end();
-  });
-};
-
 const startServer = () => {
   http
     .createServer((request, response) => {
@@ -63,76 +47,24 @@ const startServer = () => {
           response.write(html);
           response.end();
         });
-      } else if (request.url === "/animals") {
-        response.writeHead(200);
-        response.write(JSON.stringify(getAnimals("photo/reference/animal")));
-        response.end();
-      } else if (request.url.indexOf("/results") > -1) {
-        let params;
-        const sParams = request.url.split("?")[1];
-        if (sParams) {
-          const liParams = sParams.split("&").map((item) => item.split("="));
-          params = Object.fromEntries(liParams);
-        }
-        if (params.hasOwnProperty("dirs")) {
-          getFiles("photo/reference/character").then((characters) => {
-            const dirs = params.dirs.split("-");
-            getAnimalsFiles(response, characters, dirs);
-          });
-          // getFiles(`photo/reference/animal/${params.dirs}`).then((animals) => {
-          //   getFiles("photo/reference/character").then((characters) => {
-          //     response.writeHead(200);
-          //     response.write(JSON.stringify({ animals, characters }));
-          //     response.end();
-          //   });
-          // });
-        } else {
-          response.writeHead(404);
-          response.end();
-        }
-      } else if (request.url === "/create-folder" && request.method == "POST") {
-        let body = "";
-        request.on("data", (data) => {
-          body += data;
-        });
-        request.on("end", () => {
-          const { animalName, animalPath } = JSON.parse(body);
-          const imgPath = ((splits) =>
-            path.join(
-              __dirname,
-              decodeURIComponent(splits[splits.length - 1])
-            ))(animalPath.split(`${request.headers.host}/`));
-
-          let prevAnimalName = ((splits) =>
-            splits[splits.length - 1].split("/")[0])(imgPath.split("/animal/"));
-
-          const newImgPath = imgPath.replace(
-            `/${prevAnimalName}/`,
-            `/${animalName}/`
-          );
-
-          const dirPath = ((splits) => {
-            splits.pop();
-            return splits.join("/");
-          })(newImgPath.split("/"));
-
-          if (!existsSync(dirPath)) {
-            mkdirSync(dirPath);
-          }
-
-          rename(imgPath, newImgPath, (err) => {
-            if (err) throw err;
+      } else if (request.url === "/results") {
+        getFiles(CHARACTER_PATH).then((characters) => {
+          getFiles(ANIMAL_PATH).then((animals) => {
             response.writeHead(200);
+            response.write(JSON.stringify({ animals, characters }));
             response.end();
           });
         });
       } else {
-        const staticPath = path.join(
-          __dirname,
-          decodeURIComponent(request.url)
-        );
-        if (existsSync(staticPath)) {
-          const s = createReadStream(staticPath);
+        const animalStaticPath = path.join(ANIMAL_PATH, decodeURIComponent(request.url))
+        const characterStaticPath = path.join(CHARACTER_PATH, decodeURIComponent(request.url))
+        if (existsSync(animalStaticPath)) {
+          const s = createReadStream(animalStaticPath);
+          s.on("open", () => {
+            s.pipe(response);
+          });
+        } else if (existsSync(characterStaticPath)) {
+          const s = createReadStream(characterStaticPath);
           s.on("open", () => {
             s.pipe(response);
           });
---
+++ appback.js
@@ -0,0 +1,151 @@
+const http = require("http");
+const path = require("path");
+const os = require("os");
+
+const {
+  readFile,
+  readdirSync,
+  readdir,
+  existsSync,
+  mkdirSync,
+  createReadStream,
+  rename,
+} = require("fs");
+
+const getAnimals = (source) => {
+  return readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+};
+
+const getFiles = (source) => {
+  return new Promise((resolve, reject) => {
+    readdir(
+      decodeURIComponent(source),
+      { withFileTypes: true },
+      (err, files) => {
+        resolve(
+          files
+            .filter((file) => file.isFile() && file.name !== ".DS_Store")
+            .map((file) => `${source}/${file.name}`)
+        );
+      }
+    );
+  });
+};
+
+const getIndexPage = () => {
+  return new Promise((resolve, reject) => {
+    readFile("./index.html", (err, html) => {
+      if (err) throw err;
+      resolve(html);
+    });
+  });
+};
+
+const getAnimalsFiles = (response, characters, dirs) => {
+  let animals = [];
+  const results = dirs.map((dir) => getFiles(`photo/reference/animal/${dir}`));
+  Promise.all(results).then((values) => {
+    values.forEach((value) => animals.push(...value));
+    response.writeHead(200);
+    response.write(JSON.stringify({ animals, characters }));
+    response.end();
+  });
+};
+
+const startServer = () => {
+  http
+    .createServer((request, response) => {
+      if (request.url === "/") {
+        getIndexPage().then((html) => {
+          response.writeHead(200);
+          response.write(html);
+          response.end();
+        });
+      } else if (request.url === "/animals") {
+        response.writeHead(200);
+        response.write(JSON.stringify(getAnimals("photo/reference/animal")));
+        response.end();
+      } else if (request.url.indexOf("/results") > -1) {
+        let params;
+        const sParams = request.url.split("?")[1];
+        if (sParams) {
+          const liParams = sParams.split("&").map((item) => item.split("="));
+          params = Object.fromEntries(liParams);
+        }
+        if (params.hasOwnProperty("dirs")) {
+          getFiles("photo/reference/character").then((characters) => {
+            const dirs = params.dirs.split("-");
+            getAnimalsFiles(response, characters, dirs);
+          });
+          // getFiles(`photo/reference/animal/${params.dirs}`).then((animals) => {
+          //   getFiles("photo/reference/character").then((characters) => {
+          //     response.writeHead(200);
+          //     response.write(JSON.stringify({ animals, characters }));
+          //     response.end();
+          //   });
+          // });
+        } else {
+          response.writeHead(404);
+          response.end();
+        }
+      } else if (request.url === "/create-folder" && request.method == "POST") {
+        let body = "";
+        request.on("data", (data) => {
+          body += data;
+        });
+        request.on("end", () => {
+          const { animalName, animalPath } = JSON.parse(body);
+          const imgPath = ((splits) =>
+            path.join(
+              __dirname,
+              decodeURIComponent(splits[splits.length - 1])
+            ))(animalPath.split(`${request.headers.host}/`));
+
+          let prevAnimalName = ((splits) =>
+            splits[splits.length - 1].split("/")[0])(imgPath.split("/animal/"));
+
+          const newImgPath = imgPath.replace(
+            `/${prevAnimalName}/`,
+            `/${animalName}/`
+          );
+
+          const dirPath = ((splits) => {
+            splits.pop();
+            return splits.join("/");
+          })(newImgPath.split("/"));
+
+          if (!existsSync(dirPath)) {
+            mkdirSync(dirPath);
+          }
+
+          rename(imgPath, newImgPath, (err) => {
+            if (err) throw err;
+            response.writeHead(200);
+            response.end();
+          });
+        });
+      } else {
+        const staticPath = path.join(
+          __dirname,
+          decodeURIComponent(request.url)
+        );
+        if (existsSync(staticPath)) {
+          const s = createReadStream(staticPath);
+          s.on("open", () => {
+            s.pipe(response);
+          });
+        } else {
+          getIndexPage().then((html) => {
+            response.writeHead(200);
+            response.write(html);
+            response.end();
+          });
+        }
+      }
+    })
+    .listen(8888);
+};
+
+startServer();
